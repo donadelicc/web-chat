7f35c98e3ee0f071a3cd4464c86a9d5cdea81769
Preben Andersen <preb1anders1@gmail.com>
2023-11-20 01:55:17 +0100

    minor adjustments

--- response.py
+++ response.py
@@ -1,9 +1,9 @@
+from functools import lru_cache
 import pickle
 import os
 from dotenv import load_dotenv
 
-from web_scrape import fetch_website_content, get_internal_links, scrape_all_pages
-
+from performance import timeit, load_vector_store, handle_request, log_request
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -14,27 +14,49 @@ from langchain.callbacks import get_openai_callback
 from langchain.chat_models import ChatOpenAI
 
 
+## Laster inn vektordatabasen en gang til minnet
+@lru_cache(maxsize=None)  # 'None' --> ubegrenset cache-størrelse
+def get_vector_store(filename):
+    return pickle.load(open(filename, "rb"))
+
+
+
+def modify_query_with_instruction(query, instruction):
+    return f"{instruction}\n\n{query}"
+
+
+intruction = """
+    Vær en høflig chatbot assistent som representere COAX AS på best mulig vis. 
+    Ditt formål er å hjelpe kunden med å finne svar på spørsmål de måtte ha.
+    Du skal ikke gi kunden informasjon som ikke er relevant for spørsmålet de stiller.
+    Du skal ikke gi kunden informasjon som ikke er relevant for COAX AS.
+    Avslutt alle samtaler med "Er det noe mer du lurer på?".
+    Svar på følgende spørsmål:
+    """
 
 def get_response(query, VectorStoreFile):
 
     load_dotenv()
 
-    VectorStore = pickle.load(open(VectorStoreFile, "rb"))
+    ## Laster inn vektordatabasen fra minnet i stedet for å laste den inn fra fil på disk
+    VectorStore = get_vector_store(VectorStoreFile)
     
-    docs = VectorStore.similarity_search(query, k=3) ## k = number of results - top 3
+    ## Søker i vektordatabasen etter de 3 mest relevante dokumentene som matcher spørringen
+    docs = VectorStore.similarity_search(query, k=3) ## k kan endres for å få flere eller færre dokumenter
+
+    modified_query = modify_query_with_instruction(query,intruction)
+
 
     llm = ChatOpenAI(
         model_name="gpt-3.5-turbo", 
         temperature=0)
 
-    
+    ## Laster inn en kjede som kan svare på spørsmål
     chain = load_qa_chain(llm=llm, chain_type="stuff")
     
     with get_openai_callback() as callback:
-        response = chain.run(input_documents=docs, question=query)
-    return response     ## kan returnere callback her ogsså, for å få mer info om kostnader og tokens tilknyttet svaret
+        response = chain.run(input_documents=docs, question=modified_query)
+    return response, callback 
 
-
-#text = get_response("Hva er coax?", "COAX_web_content.pkl")
-#print(text)
-#embedd_text(text)
+#response, callback = get_response("Hva er coax?", "COAX_web_content.pkl")
+#print((callback.total_cost))
--- web_content.py
+++ web_content.py
@@ -1,17 +1,17 @@
 import os
 from dotenv import load_dotenv
-from azure.storage.blob import BlobServiceClient, BlobClient
 import pickle
 from io import BytesIO
 
 from web_scrape import fetch_website_content, get_internal_links, scrape_all_pages
+from performance import timeit, load_vector_store, handle_request, log_request
 
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-def embedd_and_store_text(text):
+def embedd_and_save(text):
 
     load_dotenv()
 
@@ -28,27 +28,12 @@ def embedd_and_store_text(text):
     embeddings = OpenAIEmbeddings()
     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
-    #with open("COAX_web_content.pkl", "wb") as f:
-    #    pickle.dump(VectorStore, f)
-
-    # Konverter VectorStore til en BytesIO-objekt
-    pkl_bytes = BytesIO()
-    pickle.dump(VectorStore, pkl_bytes)
-    pkl_bytes.seek(0)
-
-    # Azure Blob Storage-tilkoblingsdetaljer
-    connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    container_name = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
-    blob_name = "COAX_web_content.pkl"
-
-    # Opprett en BlobServiceClient
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-    # Laste opp til Azure Blob Storage
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    blob_client.upload_blob(pkl_bytes, blob_type="BlockBlob", overwrite=True)
+    with open("COAX_web_content.pkl", "wb") as f:
+        pickle.dump(VectorStore, f)
 
 
-#text = scrape_all_pages("https://www.coax.no/") ## få GPT til å lage en ny og mer sammenhengende tekst?
+
+
+text = scrape_all_pages("https://www.coax.no/") ## få GPT til å lage en ny og mer sammenhengende tekst?
 #print(text)
-#embedd_text(text)
+embedd_and_save(text)
